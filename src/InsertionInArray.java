c6fc8e760dee936c9ad406cbefe641fc4bddd68a
SyabAhmad <syabblogger@gmail.com>
2023-02-18 15:13:07 +0500

    Insertion in Array

--- src/InsertionInArray.java
+++ src/InsertionInArray.java
@@ -24,8 +24,44 @@ public class InsertionInArray {
             System.out.print(array[i] + ", ");
         }
         System.out.println();
-        System.out.println("Insertion at End");
-        System.out.println("Insertion at head");
 
+
+
+
+        System.out.println("Insertion at End");
+        System.out.println("First we will create a new array");
+        int[] newArray = new int[array.length + 1];
+        for (int i = 0; i < array.length;i++)
+        {
+            newArray[i] = array[i];
+            newArray[i + 1] = 22;
+        }
+        array = newArray;
+        System.out.println("New array after adding at End");
+        for (int i=0;i<array.length;i++)
+        {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println();
+
+
+
+
+
+        System.out.println("Insertion at head");
+        int[] newArray1 = new int[array.length+1];
+        int value1 = 2003;
+        newArray1[0] = value1;
+        for (int i = 0; i < array.length;i++)
+        {
+            newArray1[i+1] = array[i];
+        }
+        array = newArray1;
+        System.out.println("after insertion at head");
+        for (int i = 0; i < array.length;i++)
+        {
+            System.out.print(array[i] + ", ");
+        }
+        System.out.println();
     }
 }

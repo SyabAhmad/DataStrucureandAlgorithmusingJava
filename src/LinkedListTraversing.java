7d7a86cfa0feb7772f025096e6d7a21ee3687b2f
SyabAhmad <syabblogger@gmail.com>
2023-02-18 23:27:27 +0500

    Linked list traversing again practice

--- src/LinkedListTraversing.java
+++ src/LinkedListTraversing.java
@@ -1,15 +1,31 @@
-public class LinkedListTraversing {
+public class LinkedListTraversing{
     public static void main(String[]args)
     {
-        int[] data = {1,2,3,4,5,6,7};
-        int[] pointer = {1,2,3,4,5,6,-1};
-        int current = 0;
-        while(current != -1)
+        int[] data = {1,2,3,4,5,5,56,6,};
+        int[] indexes = {1,2,3,4,5,6,7,-1};
+        int currentValue = 0;
+        while(currentValue != -1)
         {
-            System.out.print(data[current] + ", ");
-            current = pointer[current];
+            System.out.print(data[currentValue] + ", ");
+            currentValue = indexes[currentValue];
         }
         System.out.println();
-
     }
 }
+
+
+//public class LinkedListTraversing {
+//    public static void main(String[]args)
+//    {
+//        int[] data = {1,2,3,4,5,6,7};
+//        int[] pointer = {1,2,3,4,5,6,-1};
+//        int current = 0;
+//        while(current != -1)
+//        {
+//            System.out.print(data[current] + ", ");
+//            current = pointer[current];
+//        }
+//        System.out.println();
+//
+//    }
+//}

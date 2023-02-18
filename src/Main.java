3f4fe6e15ed95d61ceab7f1eda12f36bd8f0a788
SyabAhmad <syabblogger@gmail.com>
2023-02-18 14:31:08 +0500

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Data Structure and Algorithm using Java");
+        System.out.println("All Topics we should cover in this Chapter");
+        System.out.println("----------------------------------------------");
+        System.out.println("1: Array Traversing ( 1D )");
+        System.out.println("     1.1: Array Traversing for Integer Datatype ( 1D )");
+        System.out.println("     1.2: Array Traversing for String Datatype ( 1D )");
+        System.out.println("1: Array Traversing ( 2D )");
+        System.out.println("     1.1: Array Traversing for Integer Datatype ( 2D )");
+        System.out.println("     1.2: Array Traversing for String Datatype ( 2D )");
+        System.out.println("1: addition of Two Matrix ( 2D )");
+    }
+}

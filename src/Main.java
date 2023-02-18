05842f876ae28345979af2fa48c821d6c4e1a010
SyabAhmad <syabblogger@gmail.com>
2023-02-18 15:15:26 +0500

    Exercise 4 is added

--- src/Main.java
+++ src/Main.java
@@ -6,9 +6,13 @@ public class Main {
         System.out.println("1: Array Traversing ( 1D )");
         System.out.println("     1.1: Array Traversing for Integer Datatype ( 1D )");
         System.out.println("     1.2: Array Traversing for String Datatype ( 1D )");
-        System.out.println("1: Array Traversing ( 2D )");
+        System.out.println("2: Array Traversing ( 2D )");
         System.out.println("     1.1: Array Traversing for Integer Datatype ( 2D )");
         System.out.println("     1.2: Array Traversing for String Datatype ( 2D )");
-        System.out.println("1: addition of Two Matrix ( 2D )");
+        System.out.println("3: addition of Two Matrix ( 2D )");
+        System.out.println("4: Insertion in Aray");
+        System.out.println("     4.1: at Specific Location or index");
+        System.out.println("     4.2: at Head");
+        System.out.println("     4.2: at end");
     }
 }

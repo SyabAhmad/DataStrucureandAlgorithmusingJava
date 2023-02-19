7db62d8ec1c2a7f3015081bcac56061cd196389d
SyabAhmad <syabblogger@gmail.com>
2023-02-19 11:41:51 +0500

    deletion at head and end solved

--- src/DeletionInLinkedList.java
+++ src/DeletionInLinkedList.java
@@ -7,7 +7,19 @@ public class DeletionInLinkedList {
         int current;
         for(current = 0; current < data.length-1; current++)
         {
-            System.out.println(data[current]);
+            System.out.print(data[current]+ ", ");
         }
+        //// deletion at beginning
+        System.out.println();
+        int[] data1 = {1,2,3,4,5,6,7};
+        int[] indexed1 = {1,2,3,4,5,6,-1};
+        int[] newData1 = new int[data1.length -1];
+        int current1 = 1;
+        while(current1 != -1)
+        {
+            System.out.print(data1[current1] + ", ");
+            current1 = indexed1[current1];
+        }
+        System.out.println();
     }
 }
